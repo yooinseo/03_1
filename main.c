a30ce9ac49ded3ea0bbb3903a13f3a4d5f72cdff
yooinseo <qetu37@sookmyung.ac.kr>
2025-09-21 14:46:01 +0900

    02

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    int input_int;
-    float input_float;
+    char c;
+    int i;
     
-    printf("enter an integer : ");
-    scanf("%d", &input_int);
+    printf("input a number : ");
+    scanf("%c", &c);
     
-    printf("enter a float : ");
-    scanf("%f", &input_float);
+    i = c - '0';
+    printf("The input number is %i\n", i);
     
-    printf("integer : %d, float : %f\n", input_int, input_float);
     
     system("PAUSE");	
     return 0;
