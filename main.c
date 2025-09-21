1c996c6d177f1506ccac2994c63fceb17dddf989
yooinseo <qetu37@sookmyung.ac.kr>
2025-09-21 15:21:30 +0900

    04

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    char c, d;
+    int ja, mo;
     
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("enter bunja:");
+    scanf("%d", &ja);
     
-    d = c + 1;
-    printf("The next character of %c (%i) is %c (%i).\n",c, c, d, d);
+    printf("enter bunmo:");
+    scanf("%d", &mo);
+    
+    printf("The result is %f\n", (float)ja/mo);
     
     system("PAUSE");	
     return 0;
