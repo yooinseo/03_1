a5091de6b3e070fecee76a8a6646824114d7f5a3
yooinseo <qetu37@sookmyung.ac.kr>
2025-09-21 15:09:47 +0900

    03

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    char c;
-    int i;
+    char c, d;
     
-    printf("input a number : ");
+    printf("enter a character : ");
     scanf("%c", &c);
     
-    i = c - '0';
-    printf("The input number is %i\n", i);
-    
+    d = c + 1;
+    printf("The next character of %c (%i) is %c (%i).\n",c, c, d, d);
     
     system("PAUSE");	
     return 0;
